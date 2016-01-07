37e5d2b617089f3413604206e39376b4305bcdf0
ramirezg <gabriel.ramirez@gsa.gov>
2016-01-07 09:58:14 -0500

    creating a Python client for cf

--- tests/features/steps/cloudfoundry/__init__.py
+++ tests/features/steps/cloudfoundry/__init__.py
@@ -2,18 +2,15 @@ import time
 import requests
 
 
-class CloudFoundry:
-
-    """ Script for connecting to a Clound Foundry url and requesting data """
+class CloudFoundryClient:
 
     def __init__(self, url, username, password):
-
         self.url = url
         self.username = username
         self.password = password
-        self.request_token()
+        self._request_token()
 
-    def request_token(self):
+    def _request_token(self):
         """ Request a token from service """
         token_url = 'https://uaa.%s/oauth/token' % self.url
         headers = {
@@ -29,29 +26,32 @@ class CloudFoundry:
         self.token = r.json()
         self.token['time_stamp'] = time.time()
 
-    def prepare_token(self):
+    def _prepare_token(self):
         """ Check if token is expired and open access token """
         time_elapsed = time.time() - self.token['time_stamp']
         if time_elapsed > self.token['expires_in']:
             self.request_token()
         return self.token['access_token']
 
-    def make_request(self, endpoint):
+    def _request(self, endpoint, method='get', **kwargs):
         """ Make request to specific endpoint """
-        token = self.prepare_token()
+        token = self._prepare_token()
         url = 'https://api.{0}{1}'.format(self.url, endpoint)
         headers = {'authorization': 'bearer ' + token}
-        req = requests.get(url=url, headers=headers)
+        params = kwargs.get('params')
+        if method == 'get':
+            req = requests.get(url=url, headers=headers, params=params)
+        elif method == 'post':
+            req = requests.post(url=url, headers=headers, data=kwargs['data'])
+        elif method == 'put':
+            req = requests.put(url=url, headers=headers, data=kwargs['data'])
+        elif method == 'delete':
+            req = requests.delete(url=url, headers=headers, params=params)
         return req.json()
 
     def yield_request(self, endpoint):
         """ Yield all of the request pages """
         while endpoint:
-            req = self.make_request(endpoint=endpoint)
+            req = self._request(endpoint=endpoint)
             endpoint = req.get('next_url')
             yield req
-
-    def get_user_guid(self):
-        """ Get the guid of the user """
-        data = self.make_request('/v2/info')
-        return data.get('user')
