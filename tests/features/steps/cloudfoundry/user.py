a966e92b9ede6ed41dc68bc4e143f9f348cdfbff
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-01-07 18:31:19 -0500

    adding user class for user methods

---
+++ tests/features/steps/cloudfoundry/user.py
@@ -0,0 +1,16 @@
+
+class User:
+
+    def __init__(self, guid, client):
+        self.guid = guid
+        self.client = client
+    
+    def delete_user(self):
+        """ Delete this Cloud Foundry user """
+        # Delete from Cloud Controller        
+        api_delete_endpoint = '/v2/users/%s' % self.guid
+        api_user_del_res = self.client.api_request(endpoint=api_delete_endpoint, method='delete')
+        # Delete from UAA
+        uaa_delete_endpoint = '/Users/%s' % self.guid
+        uaa_user_del_res = self.client.uaa_request(endpoint=uaa_delete_endpoint, method='delete')
+        
