128a9043cf3273f7556e6d1aea4ec39b7c5467a9
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-01-15 17:12:26 -0500

    adding features to cover ASGs for the client

--- tests/features/cloudfoundry/app.py
+++ tests/features/cloudfoundry/app.py
@@ -1,3 +1,4 @@
+import json
 
 class App:
 
@@ -5,6 +6,39 @@ class App:
         self.guid = guid
         self.client = client
 
+    def get_routes(self):
+        api_routes_endpoint = '/v2/apps/%s/routes' % self.guid
+        routes = self.client.api_request(
+            endpoint=api_routes_endpoint,
+        ).json()
+        return routes['resources']
+
+    def change_state(self, state):
+        app_endpoint = '/v2/apps/%s' % self.guid
+        return self.client.api_request(
+            endpoint=app_endpoint,
+            method='put',
+            data=json.dumps({'state': state}),
+            # params={'async': 'true'}
+        ).json()
+
+    def start(self):
+        return self.change_state('STARTED')
+
+    def stop(self):
+        return self.change_state('STOPPED')
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def restage(self):
+        app_endpoint = '/v2/apps/%s/restage' % self.guid
+        return self.client.api_request(
+            endpoint=app_endpoint,
+            method='post',
+        ).json()
+
     def delete(self):
         api_delete_endpoint = '/v2/apps/%s' % self.guid
-        api_user_del_res = self.client.api_request(endpoint=api_delete_endpoint, method='delete')
+        return self.client.api_request(endpoint=api_delete_endpoint, method='delete')
