2d74c818de9df50bb49a3e222606ccbb571522fa
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-02-22 14:54:39 -0500

    Updating UAA feature language

--- tests/CloudFoundry/UAA.feature
+++ tests/CloudFoundry/UAA.feature
@@ -1,6 +1,12 @@
-Feature: UAA Features
+Feature: CloudFoundry User Account and Authentication (UAA) Server Features
 
-  Scenario: Account Lockout (AC-7)
+  Scenario: Accounts are not logged out after 5 failed attempts to login
+    Given I am a user that can login
+      when I attempt to login 3 times and fail
+      then I am not locked out
+
+  Scenario: Accounts are logged out after 6 failed attempts to login
     Given I am a user that can login
       when I attempt to login 6 times and fail
       then I am locked out
+
--- tests/CloudFoundry/steps/UAA.py
+++ tests/CloudFoundry/steps/UAA.py
@@ -14,11 +14,6 @@ ADMIN = Client(
 )
 
 
-# Givens
-@given('I am user a master account')
-def step_impl(context):
-    context.user = ADMIN
-
 @when('I look at the audit logs')
 def step_impl(context):
     context.sample_log = context.user.events()['resources'][0]
@@ -45,3 +40,29 @@ def step_impl(context):
 def step_impl(context):
     print(context.sample_log)
     assert 'actee' in context.sample_log['entity']
+
+
+@then('I am locked out')
+def step_impl(context):
+    # Attempt to correctly login
+    user = Client(
+        api_url=config.API_URL,
+        username=config.TEST_USER,
+        password=config.TEST_USER_PASSWORD,
+        verify_ssl=False
+    )
+    assert not user.is_logged_in()
+    ADMIN.get_user(config.TEST_USER).delete()
+
+
+@then('I am not locked out')
+def step_impl(context):
+    # Attempt to correctly login
+    user = Client(
+        api_url=config.API_URL,
+        username=config.TEST_USER,
+        password=config.TEST_USER_PASSWORD,
+        verify_ssl=False
+    )
+    assert user.is_logged_in()
+    ADMIN.get_user(config.TEST_USER).delete()
