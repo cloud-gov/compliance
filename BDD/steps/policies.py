ed026433089496e8bb6480f4c340451c00e52b28
Peter Burkholder <pburkholder@pobox.com>
2016-06-15 12:50:34 -0400

    Fix BDD tests by using requests instead of urllib

--- BDD/steps/policies.py
+++ BDD/steps/policies.py
@@ -1,18 +1,15 @@
 from behave import given, then
-from urllib import request
-
+import requests
 import datetime
-
 
 @given('the github link - {url}')
 def step_impl(context, url):
     context.url = url
 
-
 @then('the policy has been updated within the last {days} days')
 def step_impl(context, days):
-    res = request.urlopen(context.url)
+    res = requests.get(context.url)
     last_modified = datetime.datetime.strptime(
-        res.getheader(name='Last-Modified'), "%a, %d %b %Y %H:%M:%S %Z")
+        res.headers.get('Last-Modified'), "%a, %d %b %Y %H:%M:%S %Z")
     x_days_ago = datetime.datetime.now() - datetime.timedelta(days=int(days))
     assert last_modified > x_days_ago
