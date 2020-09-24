ad14b4af354508c1d7c3a5de96bf60b1836209c8
Peter Burkholder <peter.burkholder@gsa.gov>
2020-09-24 18:17:32 -0400

    Id staging buckets, better names, fix timing issue

--- audit/aws-iam/controls/roles_policies.rb
+++ audit/aws-iam/controls/roles_policies.rb
@@ -3,24 +3,32 @@
 # aip.where(policy_name: /cg-s3.*/).
 # notcgs3=aip.where{ policy_name !~ /^cg-s3/ && arn !~ /::aws:policy/ }
 
-s3_iam_pattern='^cg-s3-'
+#s3_iam_pattern='^cg-s3-84'
+s3_iam_pattern='^cg-s3'
 
 aip = aws_iam_policies.where(policy_name: /#{s3_iam_pattern}/)
+s3_policy_count = aip.count
 control '1.0-s3-iams' do
     impact 1.0
     title 'Ensure s3 iams are only s3'
-    aip.entries.each do | policy|
-        policy = aws_iam_policy(policy.policy_name)
+
+    describe aws_iam_users.where(user_name: /#{s3_iam_pattern}/) do
+      its('count') { should cmp s3_policy_count }
+    end
+
+    aip.entries.each do | p|
+        policy = aws_iam_policy(p.policy_name)
         describe policy do
+            it { should be_attached_to_user(p.policy_name) }
             it { should have_statement(
                     Resource: [
-                        /^arn:aws-us-gov:s3:::cg-[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$/
+                        /^arn:aws-us-gov:s3:::(staging-)?cg-[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$/
                     ]
                 )
             }
             it { should have_statement(
                     Resource: [
-                        /^arn:aws-us-gov:s3:::cg-[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}\/\*$/
+                        /^arn:aws-us-gov:s3:::(staging-)?cg-[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}\/\*$/
                     ]
                 )
             }
@@ -28,9 +36,4 @@ control '1.0-s3-iams' do
         end
     end
 
-    s3_policy_count = aip.count
-
-    describe aws_iam_users.where(user_name: /#{s3_iam_pattern}/) do
-      its('count') { should cmp s3_policy_count }
-    end
 end
