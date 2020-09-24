7108f6508318625acde6c510e68a751ddecda714
Peter Burkholder <peter.burkholder@gsa.gov>
2020-09-24 18:17:58 -0400

    Fixed naming

--- audit/aws-iam/controls/roles_policies.rb
+++ audit/aws-iam/controls/roles_policies.rb
@@ -1,13 +1,11 @@
 # preload AWS API calls
-# aip = aws_iam_policies
-# aip.where(policy_name: /cg-s3.*/).
 # notcgs3=aip.where{ policy_name !~ /^cg-s3/ && arn !~ /::aws:policy/ }
 
 #s3_iam_pattern='^cg-s3-84'
 s3_iam_pattern='^cg-s3'
 
-aip = aws_iam_policies.where(policy_name: /#{s3_iam_pattern}/)
-s3_policy_count = aip.count
+s3_policies = aws_iam_policies.where(policy_name: /#{s3_iam_pattern}/)
+s3_policy_count = s3_policies.count
 control '1.0-s3-iams' do
     impact 1.0
     title 'Ensure s3 iams are only s3'
@@ -16,7 +14,7 @@ control '1.0-s3-iams' do
       its('count') { should cmp s3_policy_count }
     end
 
-    aip.entries.each do | p|
+    s3_policies.entries.each do | p|
         policy = aws_iam_policy(p.policy_name)
         describe policy do
             it { should be_attached_to_user(p.policy_name) }
