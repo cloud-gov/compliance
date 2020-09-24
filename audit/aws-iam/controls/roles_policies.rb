6a54546ec52dc565f1685d3b3d3e3b8313fe92a1
Peter Burkholder <peter.burkholder@gsa.gov>
2020-09-24 15:09:31 -0400

    Fixed for rolling through subset

--- audit/aws-iam/controls/roles_policies.rb
+++ audit/aws-iam/controls/roles_policies.rb
@@ -3,13 +3,14 @@
 # aip.where(policy_name: /cg-s3.*/).
 # notcgs3=aip.where{ policy_name !~ /^cg-s3/ && arn !~ /::aws:policy/ }
 
-aip = aws_iam_policies.where(policy_name: /^cg-s3-0/)
+s3_iam_pattern='^cg-s3-0'
+
+aip = aws_iam_policies.where(policy_name: /#{s3_iam_pattern}/)
 control '1.0-s3-iams' do
     impact 1.0
     title 'Ensure s3 iams are only s3'
-    aip.entries.each do | policy_name |
-        policy_name = "cg-s3-000a0b6a-e9ca-46d5-9040-98710b153a67"
-        policy = aws_iam_policy(policy_name)
+    aip.entries.each do | policy|
+        policy = aws_iam_policy(policy.policy_name)
         describe policy do
             it { should have_statement(
                     Resource: [
@@ -26,4 +27,18 @@ control '1.0-s3-iams' do
             its('statement_count') { should cmp 2 }
         end
     end
+
+    s3_policy_count = aip.count
+
+    describe aws_iam_users.where(user_name: /#{s3_iam_pattern}/) do
+      its('count') { should cmp s3_policy_count }
+    end
+
+
+
 end
+
+
+s3_policy_count = aip.count
+
+# count of s3 users should match o
